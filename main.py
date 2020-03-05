11c76210fa7b9cc7316eb172fec93eaea4fc8af1
Nizamuddin <nizamphoenix@gmail.com>
2020-03-05 23:08:34 +1100

    created a separate function get_edd_loader()

--- main.py
+++ main.py
@@ -7,28 +7,29 @@ import torch.optim as optim
 from torch.optim import lr_scheduler
 from meditorch.nn import Trainer
 from meditorch.utils.plot import plot_image_truemask_predictedmask
+import numpy as np
+
+def get_edd_loader(path,validation_split=.25,shuffle_dataset=True):
+    dataset = EDD(path)#instantiating the data set.
+    dataset_size = 100#len(dataset)
+    indices = list(range(dataset_size))
+    split = int(np.floor(validation_split * dataset_size))
+    if shuffle_dataset :
+        np.random.shuffle(indices)
+    train_indices, val_indices = indices[split:], indices[:split]
+    train_sampler = SubsetRandomSampler(train_indices)
+    valid_sampler = SubsetRandomSampler(val_indices)
+    loader={
+        'train':DataLoader(dataset, batch_size=1, sampler=train_sampler),
+        'val':DataLoader(dataset, batch_size= 5,sampler=valid_sampler)
+    }
+    return loader
+    
 
 def main():
+  np.random.seed(42)
   #seting up the data set
-  dataset = EDD()
-  #splitting the data set into train,val
-  batch_size = 5
-  validation_split = .2
-  shuffle_dataset = True
-  random_seed= 42
-  dataset_size = len(dataset)
-  indices = list(range(dataset_size))
-  split = int(np.floor(validation_split * dataset_size))
-  if shuffle_dataset :
-      np.random.seed(random_seed)
-      np.random.shuffle(indices)
-  train_indices, val_indices = indices[split:], indices[:split]
-  train_sampler = SubsetRandomSampler(train_indices)
-  valid_sampler = SubsetRandomSampler(val_indices)
-  loader={
-      'train':DataLoader(dataset, batch_size=batch_size, sampler=train_sampler),
-      'val':DataLoader(dataset, batch_size=batch_size,sampler=valid_sampler)
-  }
+  loader = get_edd_loader('./EDD2020/EDD2020_release-I_2020-01-15',validation_split=.25,shuffle_dataset=True)
   #using UNet+ResNet combo
   model = UNetResNet(in_channel=3, n_classes=5)
   optimizer_func = optim.Adam(model.parameters(), lr=1e-4)
