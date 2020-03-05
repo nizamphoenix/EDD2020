9c7988b6b2890cbb0464dfe542cd5ed9eff2884b
Nizamuddin <nizamphoenix@gmail.com>
2020-03-05 23:00:48 +1100

    v3
    made changes to load_image().Added .convert("RGB") for images

--- util.py
+++ util.py
@@ -3,19 +3,22 @@ import os
 import numpy as np
 from PIL import Image
 from skimage.transform import resize
-def load_image(path):
-    # returns an image of dtype int in range [0, 255]
-    return np.asarray(Image.open(path))
 
-def load_set(folder, shuffle=False):
+def load_image(path,is_mask):
+    if not is_mask:
+        return Image.open(path).convert("RGB")
+    else:
+        return Image.open(path)
+
+def load_set(folder,is_mask,shuffle=False):
+    data = []
     img_list = sorted(glob.glob(os.path.join(folder, '*.png')) + \
                       glob.glob(os.path.join(folder, '*.jpg')) + \
                       glob.glob(os.path.join(folder, '*.tif')) + \
                       glob.glob(os.path.join(folder, '*.jpeg')))
     if shuffle:
         np.random.shuffle(img_list)
-    data = []
     for img_fn in img_list:
-        img = load_image(img_fn)
+        img = load_image(img_fn,is_mask)
         data.append(img)
     return data, img_list
