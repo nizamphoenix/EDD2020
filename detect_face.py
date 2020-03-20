1fa80faf7a14b1461f41839a74541c1a7e9b204a
Nizamuddin <nizamphoenix@gmail.com>
2020-03-20 21:06:14 +1100

    Update detect_face.py

--- detect_face.py
+++ detect_face.py
@@ -617,3 +617,29 @@ class VideoReader:
             frame = frame[q:-q, :, :]
 
         return frame
+
+    
+    
+    
+    
+    def isotropically_resize_image(img, size, resample=cv2.INTER_AREA):
+    h, w = img.shape[:2]
+    if w > h:
+        h = h * size // w
+        w = size
+    else:
+        w = w * size // h
+        h = size
+
+    resized = cv2.resize(img, (w, h), interpolation=resample)
+    return resized
+
+
+def make_square_image(img):
+    h, w = img.shape[:2]
+    size = max(h, w)
+    t = 0
+    b = size - h
+    l = 0
+    r = size - w
+    return cv2.copyMakeBorder(img, t, b, l, r, cv2.BORDER_CONSTANT, value=0)
